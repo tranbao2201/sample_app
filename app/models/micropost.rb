30e1bb7a842db6a0b15d0aebc467e2e09f3c6d84
Tran Bao <tranvanbao2201@gmail.com>
2021-06-25 22:37:49 +0700

    Chapter 14 : Following users

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,6 +2,7 @@ class Micropost < ApplicationRecord
   belongs_to :user
   scope :sort_desc_by_time, ->{order(created_at: :desc)}
   scope :feed, ->(id){where("user_id = ?", id)}
+  scope :list_new_micropost, ->(list_id){where("user_id IN  (?)", list_id)}
   has_one_attached :image
   validates :user_id, presence: true
   validates :content, presence: true, length: {maximum: Settings.micropost
