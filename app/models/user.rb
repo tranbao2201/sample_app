de81e0e77070ac5b8770cfb3e25c4c1ec6a1c64e
Tran Bao <tranvanbao2201@gmail.com>
2021-06-25 22:37:49 +0700

    Chapter 9 : Advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,12 +10,33 @@ class User < ApplicationRecord
   has_secure_password
   validates :password, presence: true, length: {minimum: Settings.user.min_pass}
 
-  def self.digest string
-    cost = if ActiveModel::SecurePassword.min_cost
-             BCrypt::Engine::MIN_COST
-           else
-             BCrypt::Engine.cost
-           end
-    BCrypt::Password.create(string, cost: cost)
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create(string, cost: cost)
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_column(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  def forget
+    update_column(:remember_digest, nil)
   end
 end
